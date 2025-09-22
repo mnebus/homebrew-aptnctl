b062c4feb784df01972d67e158e938336104396d
Michael Nebus <mnebus@gmail.com>
2025-09-22 11:32:27 -0400

    release v0.1.2

--- aptnctl.rb
+++ aptnctl.rb
@@ -1,9 +1,9 @@
 class Aptnctl < Formula
   desc "Thin wrapper over MicroK8s for local Kubernetes management"
   homepage "https://github.com/mnebus/aptnctl"
-  version "0.1.1"
+  version "0.1.2"
   url "https://github.com/mnebus/aptnctl/archive/v#{version}.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" # Homebrew computes this from the tarball
+  sha256 "b88782d8619b27df047fa48cf4977c231b974a8090c72f621b969c18485ff003" # Homebrew computes this from the tarball
 
   # Explicitly depend on the Ubuntu MicroK8s formula from their tap
   depends_on "ubuntu/microk8s/microk8s" => :recommended
